8bcf919e64e975b1ee141502b8be9fda74ccc42e
handix <114307107+handix-dev@users.noreply.github.com>
2025-12-01 17:21:35 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,4 +1,4 @@
-/* =============================================================================
+/* ============================================================================
 CONFIG SUPABASE
 ============================================================================= */
 const SUPABASE_URL = '[https://hjrjcfloqdhbkpjpsdhn.supabase.co](https://hjrjcfloqdhbkpjpsdhn.supabase.co)';
@@ -235,3 +235,4 @@ showAuthMessage("Nom mis à jour ✔️", "success");
 ```
 
 }
+
